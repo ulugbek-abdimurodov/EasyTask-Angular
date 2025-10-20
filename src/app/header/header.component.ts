518ba1afa0da65ad07bd86110828b1b9a2258e19
ulugbek-abdimurodov <u.abdimurodov@studenti.unisa.it>
2025-10-20 16:54:51 +0200

    user-tab

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrl: './header.component.css',
   standalone: true
 })
 
