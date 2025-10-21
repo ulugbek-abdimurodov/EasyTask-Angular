ca0811a6087cff57640f3bf90310b1ef4e85e76d
ulugbek-abdimurodov <u.abdimurodov@studenti.unisa.it>
2025-10-21 09:20:27 +0200

    Input decorator based

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,16 +8,12 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  imagePath = computed(() => `assets/users/${this.avatar()}`);
-
-  // get imagePath() {
-  //   return `assets/users/${this.avatar}`;
-  // }
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
 
   onSelectUser() {
 
