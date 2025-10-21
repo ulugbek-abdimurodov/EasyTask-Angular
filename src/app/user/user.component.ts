290bc7d4fa957c684d5730ff8711adfa759c749c
ulugbek-abdimurodov <u.abdimurodov@studenti.unisa.it>
2025-10-21 09:17:52 +0200

    input function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,15 +8,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  imagePath = computed(() => `assets/users/${this.avatar()}`);
+
   // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
+  //   return `assets/users/${this.avatar}`;
   // }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
   };
 }
