fc1cc465790a317a818a48fbb88dca5549578569
ulugbek-abdimurodov <u.abdimurodov@studenti.unisa.it>
2025-10-21 11:48:35 +0200

    addNewTask UI

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,20 @@
 import { Component, input, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required: true}) userId?: string;
   @Input({required: true}) name?: string;
+
+  isAddingTask = false;
+  
   tasks = [
     {
       id: 't1',
@@ -41,5 +45,9 @@ export class TasksComponent {
 
   onCompleteTask(Id: string) {
     this.tasks = this.tasks.filter(task => task.id !== Id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
